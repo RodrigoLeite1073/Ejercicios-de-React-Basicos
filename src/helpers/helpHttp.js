708a43cc116c4a92b3d251c96f9902b067760346
Rodrigo Leite <rodrigoleite1073@gmail.com>
2023-03-23 23:22:20 -0500

    create helpHttp

---
+++ src/helpers/helpHttp.js
@@ -0,0 +1,13 @@
+export const helpHttp = () => {
+  const customFetch = () => {};
+  const get = () => {};
+  const post = () => {};
+  const put = () => {};
+  const del = () => {};
+  return {
+    get,
+    post,
+    put,
+    del,
+  };
+};
