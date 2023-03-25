ba0e0eee1d6896c10c9fca30a1b05769fa993ef2
Rodrigo Leite <rodrigoleite1073@gmail.com>
2023-03-25 00:07:57 -0500

    implements customFetch function

--- src/helpers/helpHttp.js
+++ src/helpers/helpHttp.js
@@ -1,5 +1,27 @@
 export const helpHttp = () => {
-  const customFetch = () => {};
+  const customFetch = (endpoint, opitions) => {
+    const defaultHeader = {
+      accept: "application/json",
+    };
+
+    const controller = new AbortController();
+    opitions.signal = controller.signal;
+    opitions.mehtod = opitions.method || "GET";
+    opitions.headers = opitions.headers
+      ? { ...defaultHeader, ...opitions.headers }
+      : defaultHeader;
+
+    opitions.body = JSON.stringify(opitions.body) || false;
+    if (!opitions.body) delete opitions.body;
+
+    console.log(opitions);
+
+    setTimeout(() => controller.abort(), 3000);
+
+    return fetch(endpoint, opitions)
+      .then(() => (res.ok ? res.json() : Promise.reject({})))
+      .catch((err) => {});
+  };
   const get = () => {};
   const post = () => {};
   const put = () => {};
